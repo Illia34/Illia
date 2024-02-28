e5761d086c7b1c9a7be58cfb9badc39e7fa677ae
илья bkmz <ila975547@gmail.com>
2024-02-28 20:16:43 +0100

    js

---
+++ homevork6/index.js
@@ -0,0 +1,9 @@
+const length = 100;
+const width = 300;
+
+function calculatearea(length, width) {
+    var area = length * width;
+    console.log("Площадь прямокутника: " + area);
+}
+
+calculatearea(length, width);
